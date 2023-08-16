496d1e17b47070bdd2e11080952d16e426179330
unknown <bhandariruchi498@gmail.com>
2023-08-16 15:55:04 +0530

    message

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import './../styles/App.css';
 
 const App = () => {
   const [itemName, setItemName] = useState('');
-  const [price, setPrice] = useState(0);
+  const [price, setPrice] = useState('');
   const [cart, setCart] = useState([]);
 
   const addItem = () => {
@@ -24,20 +24,20 @@ const App = () => {
   return (
     <div className="parent">
       <h1>Parent Component</h1>
-      <label>Item Name :</label><input type="text" onChange={(e) => setItemName(e.target.value)} value={itemName} />
-      <label>Item Price:</label><input type="number" onChange={(e) => setPrice(e.target.value)} value={price} />
+      <label>Item Name:</label><input onChange={(e) => setItemName(e.target.value)} value={itemName} />
+      <label>Item Price:</label><input onChange={(e) => setPrice(e.target.value)} value={price} />
       <button onClick={addItem}>Add Item</button>
       <div className="child">
         <h2>Child Component</h2>
-       
+        <ul>
           {cart.map((item, index) => (
             <li key={index}>
-              <span id="itemName">{item.name}</span>-
-              <span id="itemPrice">${item.price}</span>
+              <span className="itemName">{item.name}</span>
+              <span className="itemPrice">${item.price}</span>
               <button onClick={() => removeItem(index)}>Remove</button>
             </li>
           ))}
-        
+        </ul>
       </div>
     </div>
   );
