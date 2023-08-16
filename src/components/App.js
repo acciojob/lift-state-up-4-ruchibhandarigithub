e9d6e4ecfaad090ec1061307d05a6dee37c0d84b
unknown <bhandariruchi498@gmail.com>
2023-08-16 16:00:05 +0530

    message

--- src/components/App.js
+++ src/components/App.js
@@ -24,12 +24,12 @@ const App = () => {
   return (
     <div className="parent">
       <h1>Parent Component</h1>
-      <label>Item Name:</label><input onChange={(e) => setItemName(e.target.value)} value={itemName} />
-      <label>Item Price:</label><input onChange={(e) => setPrice(e.target.value)} value={price} />
+      <label>Item Name:</label><input id="itemName" onChange={(e) => setItemName(e.target.value)} value={itemName} />
+      <label>Item Price:</label><input id="itemPrice" onChange={(e) => setPrice(e.target.value)} value={price} />
       <button onClick={addItem}>Add Item</button>
       <div className="child">
         <h2>Child Component</h2>
-        <ul>
+        
           {cart.map((item, index) => (
             <li key={index}>
               <span className="itemName">{item.name}</span>
@@ -37,7 +37,7 @@ const App = () => {
               <button onClick={() => removeItem(index)}>Remove</button>
             </li>
           ))}
-        </ul>
+      
       </div>
     </div>
   );
